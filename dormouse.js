c878947c7ccce91332aa73b745699aaca8fa04e4
Zahan Malkani <zahanm@gmail.com>
2011-08-25 07:13:14 -0700

    Getting prototype working with CommonJS spec
    NOT WORKING YET
    still context issue in $super inheritence

--- dormouse.js
+++ dormouse.js
@@ -1,18 +1,26 @@
 
 var proto = require('./lib/prototype')
+  , sys = require('sys')
   , _ = require('underscore')._;
 
 var dm = Class.create({
   initialize: function() {
-    this.name = 'haha';
+    this.sname = 'haha';
   },
 
   out: function() {
-    console.log(this.name);
+    console.log(sys.inspect(this.sname));
   }
 });
 
-var c = new dm();
+var gm = Class.create(dm, {
+  out: function($super) {
+    console.log('called first');
+    $super();
+  }
+});
+
+var c = new gm();
 c.out();
 
 if (typeof window !== 'undefined' && !!window) { window.Class = Class; }
