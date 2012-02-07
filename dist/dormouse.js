e077dab15f8b5a9e10a61a6a303b07115617cf08
Zahan Malkani <zahanm@gmail.com>
2012-02-07 10:24:09 -0800

    /authenticate through setup_auth

--- dist/dormouse.js
+++ dist/dormouse.js
@@ -2110,11 +2110,9 @@ libutils.formatUrl = function(urlObj) {
 });
 
 require.define("/node_modules/dormouse/lib/auth.js", function (require, module, exports, __dirname, __filename) {
-var Authentication, Connection, Store, url,
+var Authentication, Connection, Store,
   __hasProp = Object.prototype.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
-
-url = require('url');
 
 Store = require('./store').Store;
 
@@ -2127,10 +2125,6 @@ Authentication = (function(_super) {
   function Authentication() {
     Authentication.__super__.constructor.apply(this, arguments);
   }
-
-  Authentication.is_authenticated = function() {
-    return Boolean(Store.access_token());
-  };
 
   Authentication.login_url = function(client_server) {
     var dm_server, project_id;
@@ -2146,34 +2140,25 @@ Authentication = (function(_super) {
     return "" + dm_server + "/api/v1/plugins/new_account?project_id=" + project_id + "&redirect_uri=http://" + client_server + "/authenticate";
   };
 
-  Authentication.setup_auth = function(app, server_store) {
+  Authentication.setup_auth = function(app) {
     return app.get('/authenticate', function(req, res) {
       var api_key, code, project_id;
       project_id = Store.project_id();
       api_key = Store.api_key();
-      parsed_url(url.parse(req.url, true));
-      code = parsed_url.query['code'];
+      code = req.query['code'];
       conosle.info(code);
       return this.get('/oauth/access_token.json', {
         project_id: project_id,
         api_key: api_key,
         code: code
-      }, function(r) {
-        if (server_store) {
-          server_store.access_token = r['access_token'];
-        } else {
-          Store.access_token(r['access_token']);
-        }
-        console.info(Store.access_token());
+      }, function(err, r) {
+        req.session.access_token = r['access_token'];
+        console.info(req.session.access_token);
         return this.get('/api/v1/users/current.json', {
-          access_token: Store.access_token()
-        }, function(r) {
-          if (server_store) {
-            server_store.user = r['user'];
-          } else {
-            Store.user(r['user']);
-          }
-          console.info(Store.user());
+          access_token: req.session.access_token
+        }, function(err, r) {
+          req.session.user = r['user'];
+          console.info(req.session.user);
           return res.redirect('/');
         });
       });
@@ -2185,11 +2170,6 @@ Authentication = (function(_super) {
 })(Connection);
 
 exports.Authentication = Authentication;
-
-});
-
-require.define("url", function (require, module, exports, __dirname, __filename) {
-// todo
 
 });
 
--- src/auth.coffee
+++ src/auth.coffee
@@ -1,17 +1,11 @@
 # Methods needed for authentication with Dormouse
 
-# Node dependencies
-url = require 'url'
-
 # Need constants from Store
 Store = require('./store').Store
 Connection = require('./connection').Connection
 
 # Dummy class with some getter methods
 class Authentication extends Connection
-
-  @is_authenticated: () ->
-    Boolean Store.access_token()
 
   # Returns a login url on the dormouse site
   @login_url: (client_server) ->
@@ -25,33 +19,33 @@ class Authentication extends Connection
     project_id = Store.project_id()
     "#{dm_server}/api/v1/plugins/new_account?project_id=#{project_id}&redirect_uri=http://#{client_server}/authenticate"
 
-  # passed in an `express` app, will setup paths to handle the auth
-  # *must be done server-side*
-  @setup_auth: (app, server_store) ->
+  # Setup an app _server side_ to listen for the `/authenticate` call
+  #
+  # Needs `express`, with middleware `express.session` and `express.query`
+  # activated for the current environment
+  #
+  # This is hard
+  # Set up a endpoint that talks to the oauth backend for dormouse
+  @setup_auth: (app) ->
     app.get '/authenticate', (req, res) ->
       project_id = Store.project_id()
       api_key = Store.api_key()
-      parsed_url url.parse req.url, true
-      code = parsed_url.query['code']
+      code = req.query['code']
       conosle.info code
       @get '/oauth/access_token.json', {
         project_id: project_id
         api_key: api_key
         code: code
-      }, (r) ->
-        if server_store
-          server_store.access_token = r['access_token']
-        else
-          Store.access_token r['access_token']
-        console.info Store.access_token()
+      }, (err, r) ->
+        # TODO error checking
+        req.session.access_token = r['access_token']
+        console.info req.session.access_token
         @get '/api/v1/users/current.json', {
-          access_token : Store.access_token()
-          }, (r) ->
-            if server_store
-              server_store.user = r['user']
-            else
-              Store.user r['user']
-            console.info Store.user()
-            res.redirect '/'
+          access_token : req.session.access_token
+        }, (err, r) ->
+          # TODO error checking
+          req.session.user = r['user']
+          console.info req.session.user
+          res.redirect '/'
 
 exports.Authentication = Authentication
